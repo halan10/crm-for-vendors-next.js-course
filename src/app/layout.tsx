76268115eb7e6318d3ec77ad226bff9802e2a28f
halan10 <halanmykola@gmail.com>
2024-08-27 22:37:31 +0300

    create project structure with all components

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+import './globals.css';
+import { Plus_Jakarta_Sans } from 'next/font/google';
+import Providers from '@/app/components/providers';
+
+const font = Plus_Jakarta_Sans({ subsets: ['latin'] });
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
